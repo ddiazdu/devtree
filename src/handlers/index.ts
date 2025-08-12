1ad16484651b1f0f0e7119e1e2d90c598cf5020b
Diego <diego.diazdu@gmail.com>
2025-08-11 20:35:04 -0400

    feat(auth): validar registro de usuarios y comenzar lógica de login

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,12 +2,19 @@ import { Request, Response } from "express";
 import slug from "slug";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
+import { validationResult } from "express-validator";
 
-//Asignar type a request y response
+//assing type to request and response
 export const createAccount = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  //error handling
+  let errors = validationResult(req);
 
-  /* Consultas */
+  if (!errors.isEmpty()) {
+    //get errors in array
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
 
   const userExists = await User.findOne({ email });
 
@@ -16,6 +23,8 @@ export const createAccount = async (req: Request, res: Response) => {
 
   const handleExists = await User.findOne({ handle });
 
+  // Prevent duplicate account creation by email
+
   if (userExists) {
     const error = new Error(
       "Ya hay un usuario registrado con este email " + email
@@ -23,15 +32,39 @@ export const createAccount = async (req: Request, res: Response) => {
     return res.status(409).json({ error: error.message });
   }
 
+  // Prevent duplicate account creation by handle
+
   if (handleExists) {
-    const error = new Error("Ya existe un usuario con este Nickname "+handle);
+    const error = new Error("Ya existe un usuario con este Nickname " + handle);
     return res.status(409).json({ error: error.message });
   }
 
+  //Asociar valor de request a User + hash + handle
   const user = new User(req.body);
   user.password = await hashPassword(password);
   user.handle = handle;
 
   await user.save();
   res.status(201).send("Registro creado correctamente");
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  //error handling
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    //get errors in array
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    const error = new Error("Este usuario no existe");
+    return res.status(404).json({ error: error.message });
+  }
+
+  //console.log("Desde Login");
 };
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,43 @@
 import { Router } from "express";
-import { createAccount } from "./handlers";
+import { body } from "express-validator";
+import { createAccount, login } from "./handlers";
+import { ExpressValidator } from "express-validator";
 
 const router = Router();
 
-/* Autenticación de usuarios */
-router.post("/auth/register", createAccount);
+/* User registration validations */
+router.post(
+  "/auth/register",
+
+  // Handle must not be empty
+  body("handle").notEmpty().withMessage("Error, el handle no debe estar vacío"),
+
+  // Name must not be empty
+  body("name").notEmpty().withMessage("Error, el name no debe estar vacío"),
+
+  // Email must be valid
+  body("email").isEmail().withMessage("Error, Email no valido"),
+
+  // Password must be at least 8 characters
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Error, la contraseña debe tener un minimo de 8 caracteres"),
+
+  createAccount
+);
+
+router.post(
+  "/auth/login",
+
+  // Email must be valid
+  body("email").isEmail().withMessage("Error, Email no valido"),
+
+  // Password must be at least 8 characters
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña es obligatoria"),
+
+  login
+);
 
 export default router;
